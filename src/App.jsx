7705c6c45a5a5c87331aba15646d12c44092275a
Arivarasan28 <jarivarasan2000@gmail.com>
2024-12-24 19:16:54 +0530

    created

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <p className="read-the-docs">
+        madhu-idiyappa-kadai
+      </p>
+    </>
+  )
+}
+
+export default App
